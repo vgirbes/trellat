c974cc27e8317931f20381d80a6232612ccdd7a8
vgirbes <vgirbes@gmail.com>
2015-09-20 20:54:46 +0200

    Random hash tests

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -14,7 +14,7 @@ module.exports.bootstrap = function(cb) {
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   var redis = require('redis')
-  var redisClient = redis.createClient(sails.config.redis.port, sails.config.redis.host)
-  redisClient.auth(sails.config.redis.password)
+  sails.redisClient = redis.createClient(sails.config.redis.port, sails.config.redis.host)
+  sails.redisClient.auth(sails.config.redis.password)
   cb();
 };
